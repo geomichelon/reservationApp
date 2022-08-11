06067699cbf987314ed95312543b29051bfdefca
George Michelon <geomichelon@gmail.com>
2022-08-11 17:26:15 -0300

    First Comiit

---
+++ AppHotelReservation/AppHotelReservation/AppHotelReservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppHotelReservationApp.swift
+//  AppHotelReservation
+//
+//  Created by George Michelon on 11/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AppHotelReservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
