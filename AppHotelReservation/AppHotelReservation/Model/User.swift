0bb543428cf1507c6311484b6f33e06a69440a88
George Michelon <geomichelon@gmail.com>
2022-08-12 16:11:58 -0300

    model user

---
+++ AppHotelReservation/AppHotelReservation/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  AppHotelReservation
+//
+//  Created by George Michelon on 12/08/22.
+//
+
+import Foundation
+
+struct User : Identifiable {
+    var id : Int
+    var username : String
+
+}
